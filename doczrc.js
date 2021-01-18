ade28d5e682cba240307487fa40714b2591f8d73
xiufu.wang <407399291@qq.com>
2021-01-18 09:46:30 +0800

    update docs

--- doczrc.js
+++ doczrc.js
@@ -4,7 +4,7 @@ export default {
   src: './' + (process.env.SRC_PATH || 'src'),
   files: [(process.env.SRC_PATH || 'src') + '/**/*.{md,markdown,mdx}'],
   dest: '/docs',
-  base: '/docs',
+  base: '/cmtter-lib',
   menu: [
     '介绍',
     { name: '构建', menu: ['基础库', 'React组件'] },
